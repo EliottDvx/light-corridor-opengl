1aec9e46f7ccd8fd1d60af2c99e02af1d22d40d8
coralie <coralie.bnnd@gmail.com>
2023-04-14 12:21:41 +0200

    essai wall

--- src/corridor.c
+++ src/corridor.c
@@ -1,61 +1,66 @@
-#include "draw_scene.h"
+#include "corridor.h"
 #include "3D_tools.h"
 
-void drawBase() {
+// void drawBase() {
 
-	glColor3f((235./255.), (207./255.), (52./255.));
+// 	glColor3f((235./255.), (207./255.), (52./255.));
 
-	glPushMatrix();
-		glScalef(3.,3.,0.1);
-		drawCircle();
-	glPopMatrix();
+// 	glPushMatrix();
+// 		glScalef(3.,3.,0.1);
+// 		drawCircle();
+// 	glPopMatrix();
 
-	glPushMatrix();
-		glScalef(2., 2., 10.);
-		drawCone();
-	glPopMatrix();
-}
+// 	glPushMatrix();
+// 		glScalef(2., 2., 10.);
+// 		drawCone();
+// 	glPopMatrix();
+// }
 
-void drawArm() {
-	glColor3f((245./255.), (164./255.), (66./255.));
+void drawWall() {
+	glColor3f(0.4,0.3,0.5);
 
+	glTranslatef(0.,0.,30.);
+	
+	glScalef(100, 20, 0);
 	glPushMatrix();
 
 		glPushMatrix();
-			glScalef(1.5, 1.5, 1.5);
-			drawSphere();
+			glRotatef(80, 0, 1, 0);
+			drawSquare();
 		glPopMatrix();
 
-		glPushMatrix();
-			glRotatef(-90, 1, 0, 0);
-			glScalef(1.,1.,10.);
-			drawCone();
-		glPopMatrix();
-		glPushMatrix();
-			glRotatef(90, 1, 0, 0);
-			glScalef(1.,1.,10.);
-			drawCone();
-		glPopMatrix();
+		// glPushMatrix();
+		// 	drawSquare();
+		// glPopMatrix();
+
+		// glPushMatrix();
+		// 	drawSquare();
+		// glPopMatrix();
+
+		// glPushMatrix();
+		// 	drawSquare();
+		// glPopMatrix();
+
 	glPopMatrix();
 }
 
-void drawPan() {
-	glColor3f((89./255.),(115./255.),0.);
-	glPushMatrix();
-		glScalef(3., 3., 5.);
-		glTranslatef(0, 0, -1);
-		drawCircle();
-		glBegin(GL_LINES);
-			glColor3f(1,0,0);
-			glVertex3f(cos(2*M_PI/3), sin(2*M_PI/3), 0.);
-			glVertex3f(0, 0, 1.);
-			glVertex3f(cos(2*M_PI/3*2), sin(2*M_PI/3*2), 0.);
-			glVertex3f(0, 0, 1.);
-			glVertex3f(cos(2*M_PI/3*3), sin(2*M_PI/3*3), 0.);
-			glVertex3f(0, 0, 1.);
-		glEnd();
-	glPopMatrix();
-}
+// void drawPan() {
+// 	glColor3f((89./255.),(115./255.),0.);
+// 	glPushMatrix();
+// 		glScalef(3., 3., 5.);
+// 		glTranslatef(0, 0, -1);
+// 		drawCircle();
+// 		glBegin(GL_LINES);
+// 			glColor3f(1,0,0);
+// 			glVertex3f(cos(2*M_PI/3), sin(2*M_PI/3), 0.);
+// 			glVertex3f(0, 0, 1.);
+// 			glVertex3f(cos(2*M_PI/3*2), sin(2*M_PI/3*2), 0.);
+// 			glVertex3f(0, 0, 1.);
+// 			glVertex3f(cos(2*M_PI/3*3), sin(2*M_PI/3*3), 0.);
+// 			glVertex3f(0, 0, 1.);
+// 		glEnd();
+// 	glPopMatrix();
+// }
 
 void drawFrame() {
     glBegin(GL_LINES);

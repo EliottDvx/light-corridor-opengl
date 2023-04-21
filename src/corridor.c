f2e385715005bbe94dae534143cbf3ec5fef25b0
EliottDvx <eliottdevaux@gmail.com>
2023-04-21 12:45:05 +0200

    wip raquette

--- src/corridor.c
+++ src/corridor.c
@@ -1,21 +1,6 @@
 #include "corridor.h"
 #include "3D_tools.h"
 
-// void drawBase() {
-
-// 	glColor3f((235./255.), (207./255.), (52./255.));
-
-// 	glPushMatrix();
-// 		glScalef(3.,3.,0.1);
-// 		drawCircle();
-// 	glPopMatrix();
-
-// 	glPushMatrix();
-// 		glScalef(2., 2., 10.);
-// 		drawCone();
-// 	glPopMatrix();
-// }
-
 void drawWall() {
 	glColor3f(0.4,0.3,0.5);
 	
@@ -53,24 +38,6 @@ void drawWall() {
 
 	glPopMatrix();
 }
-
-// void drawPan() {
-// 	glColor3f((89./255.),(115./255.),0.);
-// 	glPushMatrix();
-// 		glScalef(3., 3., 5.);
-// 		glTranslatef(0, 0, -1);
-// 		drawCircle();
-// 		glBegin(GL_LINES);
-// 			glColor3f(1,0,0);
-// 			glVertex3f(cos(2*M_PI/3), sin(2*M_PI/3), 0.);
-// 			glVertex3f(0, 0, 1.);
-// 			glVertex3f(cos(2*M_PI/3*2), sin(2*M_PI/3*2), 0.);
-// 			glVertex3f(0, 0, 1.);
-// 			glVertex3f(cos(2*M_PI/3*3), sin(2*M_PI/3*3), 0.);
-// 			glVertex3f(0, 0, 1.);
-// 		glEnd();
-// 	glPopMatrix();
-// }
 
 void drawFrame() {
     glBegin(GL_LINES);

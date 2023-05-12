d3fdb6b7b38b6c725715a380615651069e47d768
coralie <coralie.bnnd@gmail.com>
2023-05-12 15:17:12 +0200

    scene

---
+++ src/scene.c
@@ -0,0 +1,8 @@
+#include "scene.h"
+
+Scene *createScene(){
+    Scene *scene = (Scene*)malloc(sizeof(Scene));
+    scene->width = 10.;
+    scene->height = (scene->width / WINDOW_WIDTH) * WINDOW_HEIGHT;
+    return scene;
+}

413fc4ef8dfe862a82ff8a9b60c513fa21b4a5b3
coralie <coralie.bnnd@gmail.com>
2023-06-03 22:06:15 +0200

    bonus

--- src/bonus.c
+++ src/bonus.c
@@ -14,8 +14,8 @@ Bonus *createBonus(float z, Scene scene)
     newBonus->z = z;
     newBonus->y = 0;
 	
-    newBonus->height = 2;
-    newBonus->width = 2;
+    newBonus->height = 1;
+    newBonus->width = 1;
 
     newBonus->x = rand()%((int)scene.width/2 - (int)scene.width/4);
 
@@ -57,14 +57,11 @@ void drawBonus(Bonus *bonus){
 	bonus->colorR = -bonus->z/500+0.2;
     bonus->colorG = -bonus->z/500+0.4;
     bonus->colorB = -bonus->z/500+0.4;
-    float R = bonus->colorR;
-    float G = bonus->colorG;
-    float B = bonus->colorB;
+    
     glPushMatrix();
         glTranslatef(bonus->x, bonus->y, bonus->z);
         glRotatef(45,1,1,1);
-        //glScalef(bonus->width, bonus->height, 0);
-        drawCube(bonus->colorR, bonus->colorG, bonus->colorB);
+        drawCube(*bonus);
     glPopMatrix();
 }
 
@@ -90,4 +87,59 @@ void updateBonus(Scene *scene, BonusList *list){
 			bonus = bonus->next;
 		}
 	}
+}
+
+void drawCube(Bonus bonus){
+
+    float R = bonus.colorR;
+    float G = bonus.colorG;
+    float B = bonus.colorB;
+
+    float W = (float)bonus.width/2.;
+
+	//devant
+	glPushMatrix();
+		glColor3f(R, G, B);
+		glTranslatef(0,0,W);
+		drawSquare();
+	glPopMatrix();
+
+	//derriere
+	glPushMatrix();
+		glColor3f(R-0.1, G-0.1, B-0.1);
+		glTranslatef(0,0,-W);
+		drawSquare();
+	glPopMatrix();
+
+	//dessus
+	glPushMatrix();
+		glColor3f(R+0.03, G+0.03, B+0.03);
+		glTranslatef(0,W,0);
+        glRotatef(90,1,0,0);
+		drawSquare();
+	glPopMatrix();
+
+	//dessous
+	glPushMatrix();
+		glColor3f(R-0.05, G-0.05, B-0.05);
+		glTranslatef(0,-W,0);
+        glRotatef(90,1,0,0);
+		drawSquare();
+	glPopMatrix();
+
+	//droite
+	glPushMatrix();
+		glColor3f(R-0.02, G-0.02, B-0.02);
+		glTranslatef(W,0,0);
+        glRotatef(90,0,1,0);
+		drawSquare();
+	glPopMatrix();
+
+	//gauche
+	glPushMatrix();
+		glColor3f(R-0.02, G-0.02, B-0.02);
+		glTranslatef(-W,0,0);
+        glRotatef(90,0,1,0);
+		drawSquare();
+	glPopMatrix();
 }
